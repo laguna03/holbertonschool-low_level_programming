5219bd5da5e36551da518e9279b682a87d6acb6d
Pedro Augusto Laguna <lagunaaustore@gmail.com>
2023-10-19 15:33:35 -0400

    10-add.c

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* add - sum of 2  variables int
+* @a: - is a variable
+* @b: is a variable
+* Return: result of a+b
+*/
+int add(int a, int b)
+{
+int result;
+
+result  = a + b;
+
+return (result);
+}
