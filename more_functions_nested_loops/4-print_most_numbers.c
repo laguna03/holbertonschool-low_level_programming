2ef3283d4f4d7bf22b3a2dc01031586ef45c6411
Pedro Augusto Laguna <lagunaaustore@gmail.com>
2023-10-22 18:55:33 -0400

    4-print_most_numbers.c

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - Function that checks from 0-9 number except for 2 and 4
+ * Return: Always 0.
+ */
+
+void print_most_numbers(void)
+{
+	int c;
+
+	for (c = 48; c <  58; c++)
+{
+	if (c != 50)
+	{
+		if (c != 52)
+		{
+			_putchar(c);
+		}
+	}
+}
+_putchar ('\n');
+}
