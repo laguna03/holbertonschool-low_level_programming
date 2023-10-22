b8980b7ae456abd42fdd98b711247af00deea888
Pedro Augusto Laguna <lagunaaustore@gmail.com>
2023-10-22 01:25:12 -0400

    0-isupper.c

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _isupper -program to check for uppercase characters.
+ * @c: varible
+ * Return: Always 0.
+ */
+int _isupper(int c)
+
+{
+if (c >= 'A' && c <= 'Z')
+	{
+return (1);
+	}
+else
+	{
+return (0);
+}
+}
