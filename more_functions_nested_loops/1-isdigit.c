15992d11629b818d2ec6ba935fbb13c726341d47
Pedro Augusto Laguna <lagunaaustore@gmail.com>
2023-10-22 01:58:09 -0400

    1-isdigit.c

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _isdigit - Function that checks for digit 0-9
+ * @c: Inter to be checked
+ * Return: 1 if c is a digit, 0 else
+ */
+int _isdigit(int c)
+{
+	return (c >= '0' && c <= '9');
+}
