9b5ad2a469c37d5b5eb789e116794f0ad782aaa4
Pedro Augusto Laguna <lagunaaustore@gmail.com>
2023-10-22 12:00:54 -0400

    2-mul.c

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * mul - function to check for a digit multiplication (0 through).
+ * @a: a- Variable
+ * @b: b- Variable
+ * Return: Always 0.
+ */
+int mul(int a, int b)
+{
+	int mul;
+
+	mul = a * b;
+	return (mul);
+}
