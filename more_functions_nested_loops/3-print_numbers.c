4b8e967f8662902f4b1746885f49add1d0161ffa
Pedro Augusto Laguna <lagunaaustore@gmail.com>
2023-10-22 13:19:14 -0400

    3-print_numbers.c

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_numbers - function to check for digit (0 through)
+ * Return: Always 0.
+ */
+void print_numbers(void)
+{
+	int c;
+
+	for (c = 48; c < 58; c++)
+	{
+		_putchar(c);
+		_putchar('\n');
+	}
+}
