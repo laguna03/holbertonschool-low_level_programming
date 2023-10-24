6f1721ebeee8669ee006ab5b1c780abdd4467ec5
Pedro Augusto Laguna <lagunaaustore@gmail.com>
2023-10-24 13:13:41 -0400

    3-puts.c

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _puts - function to print an array of characters(a string) to the stdout
+ * ending with a new line.
+ * @str: array character.
+ * Return: Void
+ */
+void _puts(char *str)
+
+{
+	int  count  = 0;
+
+	while (str[count] != '\0')
+	{
+		_putchar(str[count]);
+		count++;
+	}
+	_putchar(10);
+}
