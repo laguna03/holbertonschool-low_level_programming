d151dabc57d43cb04010d74d03a818da7145695c
Pedro Augusto Laguna <lagunaaustore@gmail.com>
2023-10-29 17:59:35 -0400

    3-strspn.c

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * _strspn - Get the lengh of a prefix substring
+ * @s: array base
+ * @accept: substring
+ * Return: number of bytes in the initial segment of s
+*/
+unsigned int _strspn(char *s, char *accept)
+{
+unsigned int i, j;
+
+	for (i = 0; s[i]; i++)
+	{
+		for (j = 0; accept[j]; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				break;
+			}
+		}
+	if (!accept[j])
+	{
+		break;
+	}
+	}
+return (i);
+}
