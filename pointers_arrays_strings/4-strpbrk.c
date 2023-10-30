0b224365f84a51bac19a7a7d01effd3a6423ebc6
Pedro Augusto Laguna <lagunaaustore@gmail.com>
2023-10-30 08:43:46 -0400

    4-strpbrk.c

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stddef.h>
+/**
+ * _strpbrk - search a string
+ * @s: array original
+ * @accept: string to compare with ori
+ * Return: pointer to byte in s
+*/
+char *_strpbrk(char *s, char *accept)
+{
+int i, j;
+
+	for (i = 0; s[i]; i++)
+	{
+		for (j = 0; accept[j]; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				return (s + i);
+			}
+		}
+	}
+s = NULL;
+return (s);
+}
