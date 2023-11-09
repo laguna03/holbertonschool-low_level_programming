d3b251e0c99436110cb116a2057e3c769f7cd962
Pedro Augusto Laguna <lagunaaustore@gmail.com>
2023-11-09 12:32:14 -0400

    2-str_concat.c

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -5,48 +5,46 @@
  * @s1: first
  * @s2: second
  * Return: pos
- */
-
+*/
 char *str_concat(char *s1, char *s2)
 {
-	char *pos;
-	int a, lens1, lens2, k = 0;
+char *pos;
+int a, lens1, lens2, k = 0;
 
 if (s1 == NULL)
 {
-	lens1 = 0;
+lens1 = 0;
 }
 
 else
 {
-	lens1 = strlen(s1);
+lens1 = strlen(s1);
 }
 
 if (s2 == NULL)
 {
-	lens2 = 0;
+lens2 = 0;
 }
 
 else
 {
-	lens2 = strlen(s2);
+lens2 = strlen(s2);
 }
 
-pos = (char *)malloc(((lens1 + lens2) * sizeof(char)) + 1);
+pos = malloc(((lens1 + lens2) *sizeof(char)) + 1);
+if (pos == NULL)
+return (NULL);
+	for (a = 0; a < lens1; a++)
+	{
+		pos[a] = s1[a];
+	}
 
-for (a = 0; a < lens1; a++)
-{
-	pos[a] = s1[a];
-}
-
-for (; a < (lens1 + lens2); a++)
-{
-	pos[a] = s2[k];
+	for (; a < (lens1 + lens2); a++)
+	{
+		pos[a] = s2[k];
 	k++;
-}
+	}
 
-{
 pos[a] = '\0';
 return (pos);
-}
 }
