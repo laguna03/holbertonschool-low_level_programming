afb8a15c1d9a5a77a1a489499ad84a6ccc93346e
Pedro Augusto Laguna <lagunaaustore@gmail.com>
2023-11-11 22:49:46 -0400

    1-string_nconcat.c

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -12,38 +12,30 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-    char *p;
-    unsigned int sizes1, i, j;
+char *p;
+unsigned int sizes1;
+unsigned int i, j;
 
-    if (s1 == NULL)
-        s1 = "";
-    if (s2 == NULL)
-        s2 = "";
+if (s1 == NULL)
+s1 = "";
+if (s2 == NULL)
+s2 = "";
 
-    sizes1 = strlen(s1);
-
-    // Allocate memory for both strings and null terminator
-    p = malloc(sizes1 + n + 1);
-    if (p == NULL)
-    {
-        return (NULL);
-    }
-
-    // Copy s1 to p
-    for (i = 0; i < sizes1; i++)
-    {
-        p[i] = s1[i];
-    }
-
-    // Concatenate n characters from s2 to p
-    for (j = 0; j < n && s2[j] != '\0'; j++)
-    {
-        p[i] = s2[j];
-        i++;
-    }
-
-    // Null-terminate the result
-    p[i] = '\0';
-
-    return (p);
+sizes1 = strlen(s1);
+p = malloc((sizes1 + n) + 1);
+	if (p == NULL)
+	{
+	return (NULL);
+	}
+	for (i = 0; i < sizes1; i++)
+	{
+		p[i] = s1[i];
+	}
+	for (j = 0; j < n; j++)
+	{
+		p[i] = s2[j];
+		i++;
+	}
+p [i] = '\0';
+return (p);
 }
