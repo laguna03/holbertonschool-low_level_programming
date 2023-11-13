f246d106cd398c568005b528626fee50843919c0
Pedro Augusto Laguna <lagunaaustore@gmail.com>
2023-11-13 14:53:26 -0400

    dog.h

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,6 +1,5 @@
 #ifndef DOG
 #define DOG
-
 /**
  * struct dog - defines a dog class
  * @name: name
@@ -9,12 +8,11 @@
  *
  * Description: dog
  */
-
 struct dog
 {
-  char *name;
-  float age;
-  char *owner;
+char *name;
+float age;
+char *owner;
 };
 typedef struct dog dog_t;
 
