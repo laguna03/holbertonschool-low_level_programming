f3ecd6ab3474e198f023c30f52224d4ffd8829dc
Pedro Augusto Laguna <lagunaaustore@gmail.com>
2023-11-14 17:09:03 -0400

    0-print_name.c

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,15 +1,14 @@
 #include "function_pointers.h"
 #include <stdlib.h>
-
 /**
  * print_name - prints a nam
  * @name: name of the person
  * @f: fun
+ *
  * Return: Nothing.
  */
-
 void print_name(char *name, void (*f)(char *))
 {
-	if (name != NULL && f != NULL)
-	f(name);
+if (name != NULL && f != NULL)
+f(name);
 }
