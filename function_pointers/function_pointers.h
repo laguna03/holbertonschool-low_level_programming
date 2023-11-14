c60769dc6c6fc4f984dec1ffd9b056349d521fd1
Pedro Augusto Laguna <lagunaaustore@gmail.com>
2023-11-14 17:07:05 -0400

    0-print_name.c

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -1,8 +1,10 @@
 #include <stddef.h>
 #ifndef MAIN
 #define MAIN
+
 char _putchar(char);
 int int_index(int *array, int size, int (*cmp)(int));
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
+
 #endif
