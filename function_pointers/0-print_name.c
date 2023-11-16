f48a0cd6a877be6d5913d769295c58428d2e8f5d
Pedro Augusto Laguna <lagunaaustore@gmail.com>
2023-11-16 12:14:04 -0400

    0-print_name.c

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -4,10 +4,10 @@
  * print_name - prints a nam
  * @name: name of the person
  * @f: fun
+ *
  * Return: Nothing.
  */
 void print_name(char *name, void (*f)(char *))
-
 {
 if (name != NULL && f != NULL)
 f(name);
--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -2,7 +2,7 @@
 #ifndef MAIN
 #define MAIN
 int _putchar(char);
+int int_index(int *array, int size, int (*cmp)(int));
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
-int int_index(int *array, int size, int (*cmp)(int));
 #endif
