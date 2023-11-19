f49dcb937c62982a0bae14714d0662d3db59f8b1
Pedro Augusto Laguna <lagunaaustore@gmail.com>
2023-11-19 11:07:13 -0400

    0-sum_them_all.c

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -8,13 +8,16 @@
 */
 int sum_them_all(const unsigned int n, ...)
 {
+
 va_list ar;
-unsigned int sum, i;
+unsigned int sum;
+unsigned int i;
+sum = 0;
 
 va_start(ar, n);
 	for (i = 0; i < n; i++)
 	{
-		sum += va_arg(ar, int);
+	sum += va_arg(ar, int);
 	}
 va_end(ar);
 
