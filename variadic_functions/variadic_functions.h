301bbb983d435c81951a6e530b217b27fd37bfec
Pedro Augusto Laguna <lagunaaustore@gmail.com>
2023-11-20 11:09:40 -0400

    0-sum_them_all.c

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -8,7 +8,7 @@ void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 
 /**
- * struct s_pick  - Sum, string and all
+ * typdef struct pick  - Sum, string and all
  * @let: letters
  * @va_list: variable list
  * @pick_al: all
