8c65c3ca858445ce1b2ec964e680fbc79ae727d4
Pedro Augusto Laguna <lagunaaustore@gmail.com>
2023-11-29 21:28:51 -0400

    betty corrections

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -3,16 +3,14 @@
 #include <stdlib.h>
 
 /**
- * print_list - function that prints all elements
- * of a list
- * @list_t: single linked list
+ * print_list - prints list
  * @h: head node
  * Return: count
 */
 
 size_t print_list(const list_t *h)
+
 {
-  
 int count = 0;
 const list_t *p = h;
 
