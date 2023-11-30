751dc8c1bae90fe8bc02a08b8e5a8d4bd80a14ec
Pedro Augusto Laguna <lagunaaustore@gmail.com>
2023-11-29 21:24:22 -0400

    betty corrections

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 
 /**
- * print_list - function that prints all elements 
+ * print_list - function that prints all elements
  * of a list
  * @list_t: single linked list
  * @h: head node
