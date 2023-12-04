880ed104a11b419a16ed9447c377872462ba3a49
Pedro Augusto Laguna <lagunaaustore@gmail.com>
2023-12-03 21:05:37 -0400

    5-get_dnodeint.c

---
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+ * get_dnodeint_at_index - returns the nth node of linked list
+ * @head: header
+ * @index: data pos
+ * Return: node
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+unsigned int i = 0;
+
+	while (i < index)
+	{
+		if (!head)
+		{
+			return (NULL);
+		}
+
+		head = head->next;
+		i++;
+	}
+return (head);
+}
