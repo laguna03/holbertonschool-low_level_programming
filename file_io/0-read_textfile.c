4cb266c486a0ea859c669012c74f49cac9bd8ab4
Pedro Augusto Laguna <lagunaaustore@gmail.com>
2023-12-09 08:41:06 -0400

    0-read_textfile.c creation with code

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -10,31 +10,31 @@ ssize_t read_textfile(const char *filename, size_t letters)
 char *st;
 int op, rd, wr;
 
-	if (filename == NULL)
-		return (0);
+if (filename == NULL)
+return (0);
 
 st = malloc(letters);
 op = open(filename, O_RDONLY);
 
-	if (op == -1)
-	{
-		free(st);
-		return (0);
-	}
+if (op == -1)
+{
+free(st);
+return (0);
+}
 rd = read(op, st, letters);
-	if (rd == -1)
-	{
-		free(st);
-		return (0);
-	}
+if (rd == -1)
+{
+free(st);
+return (0);
+}
 wr = write(STDOUT_FILENO, st, rd);
 
-	if (wr == -1)
-	{
-		free(st);
-		close(op);
-		return (0);
-	}
+if (wr == -1)
+{
+free(st);
+close(op);
+return (0);
+}
 free(st);
 close(op);
 return (wr);
