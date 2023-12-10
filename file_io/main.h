0897e0482bd3c3b713cc0f4715e3a070ed735633
Pedro Augusto Laguna <lagunaaustore@gmail.com>
2023-12-10 11:43:17 -0400

    adding libraries

--- file_io/main.h
+++ file_io/main.h
@@ -2,10 +2,11 @@
 #define MAIN_H
 
 #include <stdio.h>
-#include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 #include <fcntl.h>
-#include <string.h>
+#include <stdlib.h>
+#include <sys/stat.h>
 
 char _putchar(char);
 ssize_t read_textfile(const char *filename, size_t letters);
