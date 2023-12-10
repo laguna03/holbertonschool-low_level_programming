fed467ec7670841e8ecfed4bfea05c1bc0ad0823
Pedro Augusto Laguna <lagunaaustore@gmail.com>
2023-12-10 14:25:22 -0400

    task number 4 complete

---
+++ hash_tables/4-hash_table_get.c
@@ -0,0 +1,29 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - function
+ * @ht: hash table to look
+ * @key: key to look
+ * 
+ * Return: value associated with element, or NULL if not found
+*/
+
+char *hash_table_get(const hash_table_t *ht, const char *key)
+
+{
+    unsigned long int index;
+    hash_node_t *tmp;
+
+    if (!ht || !key)
+    return (0);
+
+    index = key_index((unsigned char *)key, ht->size);
+    tmp = ht->array[index];
+    while (tmp)
+    {
+        if (strcmp(tmp->key, key) == 0)
+        return (tmp->value);
+        tmp = tmp->next;
+    }
+    return (NULL);
+}
