4a7c0306d661f4e03619dfa896ab9030e55924b5
Pedro Augusto Laguna <lagunaaustore@gmail.com>
2023-12-10 10:53:28 -0400

    1-create_file.c task completed

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -7,25 +7,25 @@
 */
 int create_file(const char *filename, char *text_content)
 {
-	int cr, wr;
+int cr, wr;
 
-	if (filename == NULL)
-		return (-1);
+if (filename == NULL)
+return (-1);
 
-	cr = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+cr = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 
-	if (cr == -1)
-		return (-1);
+if (cr == -1)
+return (-1);
 
-	if (!text_content)
-		return (1);
+if (!text_content)
+return (1);
 
-	wr = write(cr, text_content, strlen(text_content));
-	if (wr == -1)
-	{
-		close(cr);
-		return (-1);
-	}
-	close(cr);
-	return (1);
+wr = write(cr, text_content, strlen(text_content));
+if (wr == -1)
+{
+close(cr);
+return (-1);
+}
+close(cr);
+return (1);
 }
