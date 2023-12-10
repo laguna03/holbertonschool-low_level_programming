87ce46e2e1520d904b1682639db7509b40dbddbc
Pedro Augusto Laguna <lagunaaustore@gmail.com>
2023-12-10 14:26:57 -0400

    betty correction task 4

--- hash_tables/4-hash_table_get.c
+++ hash_tables/4-hash_table_get.c
@@ -4,26 +4,25 @@
  * hash_table_get - function
  * @ht: hash table to look
  * @key: key to look
- * 
  * Return: value associated with element, or NULL if not found
 */
 
 char *hash_table_get(const hash_table_t *ht, const char *key)
 
 {
-    unsigned long int index;
-    hash_node_t *tmp;
+unsigned long int index;
+hash_node_t *tmp;
 
-    if (!ht || !key)
-    return (0);
+if (!ht || !key)
+return (0);
 
-    index = key_index((unsigned char *)key, ht->size);
-    tmp = ht->array[index];
-    while (tmp)
-    {
-        if (strcmp(tmp->key, key) == 0)
-        return (tmp->value);
-        tmp = tmp->next;
-    }
-    return (NULL);
+index = key_index((unsigned char *)key, ht->size);
+tmp = ht->array[index];
+while (tmp)
+{
+if (strcmp(tmp->key, key) == 0)
+return (tmp->value);
+tmp = tmp->next;
+}
+return (NULL);
 }
